62b6f72e1839f32c62ea67f98faad9b8e970b806
mod washi <your.email@example.com>
2013-03-14 16:27:05 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assignment::Application.config.secret_token = 'ac27a5c9a563f8dec0638ff6c5cdb1cbf36601186f3a99f6d48bf9e071e5e77bfcca9008d088a4135b9d62defc1b4c83ec9c672064ae160e09a2796f7c264a2c'
